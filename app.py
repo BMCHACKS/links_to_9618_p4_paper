9fd52115902805a87ee3dfc77719fdee4a9bbae2
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2024-12-24 16:32:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,6 @@ def get_links():
     return jsonify({"links": valid_links})
 
 if __name__ == '__main__':
+    # Use the environment variable for port in production
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
